51ea14fcca4bfe131cecef36ca42ed0443784e02
Chris Souers <csouers@gmail.com>
2019-06-12 22:20:36 -0400

    stop panda flash

--- selfdrive/pandad.py
+++ selfdrive/pandad.py
@@ -4,11 +4,10 @@ import os
 from panda import ensure_st_up_to_date
 
 def main(gctx=None):
-  ensure_st_up_to_date()
+  #ensure_st_up_to_date()
 
   os.chdir("boardd")
   os.execvp("./boardd", ["./boardd"])
 
 if __name__ == "__main__":
   main()
-
